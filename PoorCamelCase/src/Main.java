29339dc0114a1cc87cc10d07d78024c431f6fdd4
michsz <michal.szagol@gmail.com>
2018-01-18 19:33:54 +0100

    Simple algorithm to change any text to CamelCase text (spaces are also printed)

---
+++ PoorCamelCase/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.print("Please type any word/sentence: ");
+        Scanner sc = new Scanner(System.in);
+        String sentence = sc.nextLine();
+
+        char [] charsArray = sentence.toCharArray();
+
+        for (int i = 0; i < charsArray.length; i++){
+            if (i == 0 || i%2==0){
+                System.out.print(Character.toString(charsArray[i]).toUpperCase());
+            }
+            else {
+                System.out.print(Character.toString(charsArray[i]).toLowerCase());
+            }
+        }
+
+
+    }
+}
